5397d8af9e59d668c1ba41ba13694bfaff32b24c
Kota Noda <kouta.tus1@gmail.com>
2022-02-13 05:35:32 +0900

    enabled POST of nodes and seq_cluster

---
+++ main/utils.py
@@ -0,0 +1,2 @@
+def printing():
+    return 'Hello World!'
--- main/views.py
+++ main/views.py
@@ -2,26 +2,27 @@ from django.shortcuts import render
 from django.conf import settings
 from django.http import JsonResponse
 
+import json
+from .utils import *
+
+from matplotlib.font_manager import json_load
+
 def index(request):
     return render(request, 'main/index.html', {})
 
 def ajax_number(request):
-    #number1 = int(request.POST.get('number1'))
-    #number2 = int(request.POST.get('number2'))
-    #poyota = int(request.POST.get('poyota'))
-    number1=1
-    number2=2
-    poyota=1
-    supernova = request.POST.getlist("supernova[]")
-    plus = number1 + number2
-    minus = number1 - number2
-    SUPER_POYOTA = poyota*1000000000 
+    print(request.POST)
+    nodes = request.POST.get("nodes")
+    seq_cluster = request.POST.get("seq_cluster")
+    nodes_dict = json.loads(nodes)
+    seq_cluster_dict = json.loads(seq_cluster)
+    a = nodes_dict['1']
+    b = seq_cluster_dict[0]
+    c = printing()
+    print(c)
     d = {
-        'plus': plus,
-        'minus': minus,
-        'poyota': SUPER_POYOTA,
+        'a':a,
+        'b':b,
+        'c':c
     }
-    print(d)
-    print(type(supernova))
-    print(f'supernova is {supernova}')
     return JsonResponse(d)
